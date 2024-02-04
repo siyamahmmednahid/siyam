a62ab8f5245d416405eb00bad7d7d84584d526ec
Siyam Ahmmed Nahid <siyam14.boy@gmail.com>
2024-02-04 23:32:14 +0600

    Site Update

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const words = ["UI/UX Designer", "Product Designer", "UX Researcher"];
 let target_word_index = 0;
 let target_char_index = 0;
 const target_element = document.getElementById("typing");
-const toggle_btn = document.getElementById("toggle_btn");
-const sidebar_list = document.getElementById("sidebarList");
+const toggleBtn = document.getElementById("toggleBtn");
+const sidebarList = document.getElementById("sidebarList");
+
+
+
+// Typing effect
 console.log(target_element);
 
 function typeWord() {
@@ -25,19 +29,56 @@ function clearOutput() {
   setTimeout(typeWord, 500);
 }
 
-toggle_btn.addEventListener("click", () => {
-  document.body.classList.toggle("color_body");
-});
-
-sidebar_list.addEventListener("click", () => {
-  // remove class name from body
-  document.body.classList.remove("color_body");
-});
-
-// on scroll event
-window.addEventListener("scroll", () => {
-  document.body.classList.remove("color_body");
-});
-
 // Start the typing animation
 typeWord();
+
+
+
+// Sidebar toggle
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("activeMenu");
+});
+
+// Remove class name from body on click
+sidebarList.addEventListener("click", () => {
+  document.body.classList.remove("activeMenu");
+});
+
+// Remove class name from body on scroll
+window.addEventListener("scroll", () => {
+  document.body.classList.remove("activeMenu");
+});
+
+
+
+// Form Submission
+var form = document.getElementById("contactForm");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
